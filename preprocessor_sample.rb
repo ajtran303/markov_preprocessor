e73d55a844f4bde5175d9c9c0de8465476800395
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-04-25 00:56:34 -0600

    Add working preprocessor_sample.rb

--- preprocessor_sample.rb
+++ preprocessor_sample.rb
@@ -2,30 +2,45 @@ class Preprocessor
 
   def initialize
     @text_file_object = File.open(ARGV[0], "r")
+    @file_name = ARGV[0][0..-5]
   end
 
+  private
+
+  # It iterates over an array of each line in the file
+
   def k_arr
-    @k_arr = @text_file_object.readlines.map do |line|
+    k_arr = @text_file_object.readlines.map do |line|
       clean(line)
     end
     @text_file_object.close
-    return @k_arr
+    return k_arr
   end
+
+  # It replaces all /n with a single space
+  # It pads all punctuation marks with single spaces
+  # It replaces all double spaces with a single space
+  # It strips leading whitespace
 
   def clean(line)
     line.gsub!("\n", " ")
     line.gsub!(/(?<foo>[[:punct:]])/, ' \k<foo> ')
-    line.gsub!("  ", " ")
+    2.times { line.gsub!("  ", " ") }
     line.lstrip
   end
+
+  # It joins the array into ONE BIG STRING
 
   def k_str
     k_arr.join
   end
 
+  public
+
+  # It writes the ONE BIG STRING into a new file
+
   def create_clip
-    name = "#{ARGV[0][0..-5]}_clipped.txt"
-    clip_file = File.open("#{name}", "w")
+    clip_file = File.open("#{@file_name}_clipped.txt", "w")
     clip_file.write(k_str)
     clip_file.close
   end
